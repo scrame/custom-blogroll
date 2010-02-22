4d3e940f31310c8e1a26e7e8c3bc1c62289072e3
screaming scramechan <scrame@scrame.com>
2010-02-21 18:14:09 -0800

    first working version

--- custom-blogroll/blogroll.php
+++ custom-blogroll/blogroll.php
@@ -1,74 +1,46 @@
 <?php
 /*
-Plugin Name: Hello Dolly
+Plugin Name: Custom Blogroll
 Plugin URI: http://wordpress.org/#
-Description: This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong: Hello, Dolly. When activated you will randomly see a lyric from <cite>Hello, Dolly</cite> in the upper right of your admin screen on every page.
-Author: Matt Mullenweg
-Version: 1.5
-Author URI: http://ma.tt/
+Description: Custom blogroll
+Author: liam
+Version: 0.1
+Author URI: 
 */
 
-// These are the lyrics to Hello Dolly
-$lyrics = "Hello, Dolly
-Well, hello, Dolly
-It's so nice to have you back where you belong
-You're lookin' swell, Dolly
-I can tell, Dolly
-You're still glowin', you're still crowin'
-You're still goin' strong
-We feel the room swayin'
-While the band's playin'
-One of your old favourite songs from way back when
-So, take her wrap, fellas
-Find her an empty lap, fellas
-Dolly'll never go away again
-Hello, Dolly
-Well, hello, Dolly
-It's so nice to have you back where you belong
-You're lookin' swell, Dolly
-I can tell, Dolly
-You're still glowin', you're still crowin'
-You're still goin' strong
-We feel the room swayin'
-While the band's playin'
-One of your old favourite songs from way back when
-Golly, gee, fellas
-Find her a vacant knee, fellas
-Dolly'll never go away
-Dolly'll never go away
-Dolly'll never go away again";
 
-// Here we split it into lines
-$lyrics = explode("\n", $lyrics);
-// And then randomly choose a line
-$chosen = wptexturize( $lyrics[ mt_rand(0, count($lyrics) - 1) ] );
+$category = 5;
+$sub_category = 16;
 
-// This just echoes the chosen line, we'll position it later
-function hello_dolly() {
-	global $chosen;
-	echo "<p id='dolly'>$chosen</p>";
+function make_link($result_map) {
+  return '<a href='
+    .'"' . $result_map['link']  .'"'
+    .'>'
+    .$result_map['title']
+    .'</a>'
+    .'<p>';
 }
 
-// Now we set that function up to execute when the admin_footer action is called
-add_action('admin_footer', 'hello_dolly');
+function custom_bookmarks() {
+  $main_posts = get_posts(array('category__and' => array(4,5)));
 
-// We need some CSS to position the paragraph
-function dolly_css() {
-	echo "
-	<style type='text/css'>
-	#dolly {
-		position: absolute;
-		top: 2.3em;
-		margin: 0;
-		padding: 0;
-		right: 10px;
-		font-size: 16px;
-		color: #d54e21;
-	}
-	</style>
-	";
+  $output = '';
+
+  foreach ( (array) $main_posts as $post) {    
+    $result;
+    $result['title'] = apply_filters('the_title', $post->post_title);
+    $result['link'] = apply_filters('the_title', $post->post_link);
+
+    $output .= make_link($result);
+  }
+
+  return $output;
+  //  return "this is a test of the custom bookmarks.";
 }
 
-add_action('admin_head', 'dolly_css');
+
+
+add_shortcode('show-bookmarks', 'custom_bookmarks')
 
 ?>
+
